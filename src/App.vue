59ac68890c488f719c360388124f6a84a4ae497d
Muhammed Fatih Celik <mfatihceliik@outlook.com>
2021-12-15 20:10:19 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Navigation />
+      <router-link to="/"></router-link> 
+      <router-link to="/register"></router-link> 
+      <router-link to="/factories"></router-link> 
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-left:auto;
+  margin-right:auto;
+  width:70%;
 }
+
+
 </style>
